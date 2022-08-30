aece4093a03afb5b6f572778904791d64b277174
Christopher Corvin <X>
2022-08-31 00:00:14 +0300

    change cell color to red on mouse click event

--- Assets/Scripts/UserControl.cs
+++ Assets/Scripts/UserControl.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class UserControl : MonoBehaviour
 {
+    private SpriteRenderer renderer;
+
     // Start is called before the first frame update
     void Start() {
-
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -15,6 +17,7 @@ public class UserControl : MonoBehaviour
     }
 
     private void OnMouseDown() {   
+        renderer.color = Color.red;
         Debug.Log(this.gameObject.tag + "[" + this.gameObject.transform.position.x + ":" + this.gameObject.transform.position.y + "]");
     }
 }
