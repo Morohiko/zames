cecc1e80bed921559684af909bd60c604a165ba4
Christopher Corvin <X>
2022-08-30 23:56:18 +0300

    print object tag and position on mouse click event

---
+++ Assets/Scripts/UserControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnMouseDown() {   
+        Debug.Log(this.gameObject.tag + "[" + this.gameObject.transform.position.x + ":" + this.gameObject.transform.position.y + "]");
+    }
+}
